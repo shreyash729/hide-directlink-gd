66ff0107ef6b34c82a4c781d7a46f7f2cf826355
shreyash729 <98596657+shreyash729@users.noreply.github.com>
2022-07-08 13:22:31 +0530

    Update generate.php

--- generate.php
+++ generate.php
@@ -41,8 +41,60 @@ if ($_POST['submit'] != "") {
 <!doctype html>
 <html lang="en">
 <head>
+    <style>
+.button-50 {
+  appearance: button;
+  background-color: #000;
+  background-image: none;
+  border: 1px solid #000;
+  border-radius: 4px;
+  box-shadow: #fff 4px 4px 0 0,#000 4px 4px 0 1px;
+  box-sizing: border-box;
+  color: #fff;
+  cursor: pointer;
+  display: inline-block;
+  font-family: ITCAvantGardeStd-Bk,Arial,sans-serif;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 20px;
+  margin: 0 5px 10px 0;
+  overflow: visible;
+  padding: 12px 40px;
+  text-align: center;
+  text-transform: none;
+  touch-action: manipulation;
+  user-select: none;
+  -webkit-user-select: none;
+  vertical-align: middle;
+  white-space: nowrap;
+}
+
+.button-50:focus {
+  text-decoration: none;
+}
+
+.button-50:hover {
+  text-decoration: none;
+}
+
+.button-50:active {
+  box-shadow: rgba(0, 0, 0, .125) 0 3px 5px inset;
+  outline: 0;
+}
+
+.button-50:not([disabled]):active {
+  box-shadow: #fff 2px 2px 0 0, #000 2px 2px 0 1px;
+  transform: translate(2px, 2px);
+}
+
+@media (min-width: 768px) {
+  .button-50 {
+    padding: 12px 50px;
+  }
+}
+    </style>
   <meta charset="utf-8" />
-    <title>GDrive Gen</title>
+   <title>FilmHit-Drive Download Generator</title>
      <link rel="stylesheet" href="https://bootswatch.com/4/litera/bootstrap.min.css">
     <style>
     .info-icon{margin-top:-6px}
@@ -90,7 +142,7 @@ if ($iframeid) {
         <h2>CREATED WITH LOVE BY CODER729</h2>
         <a href="<?php
     echo 'https://' . $_SERVER['SERVER_NAME'] . '?id=' . $iframeid;
-?>"><button class="btn btn-default">Download</button></a></center>
+?>"><button class="button-50">Download Now</button></a></center>
         <?php
 }
 ?>
