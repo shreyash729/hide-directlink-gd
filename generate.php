45b295706f62ed37c2478faeaf8175e9ba0e5873
shreyash729 <98596657+shreyash729@users.noreply.github.com>
2022-07-08 13:33:47 +0530

    Update generate.php

--- generate.php
+++ generate.php
@@ -42,6 +42,107 @@ if ($_POST['submit'] != "") {
 <html lang="en">
 <head>
     <style>
+        .input {
+	
+	// needs to be relative so the :focus span is positioned correctly
+	position:relative;
+	
+	// bigger font size for demo purposes
+	font-size: 1.5em;
+	
+	// the border gradient
+	background: linear-gradient(21deg, #10abff, #1beabd);
+	
+	// the width of the input border
+	padding: 3px;
+	
+	// we want inline fields by default
+	display: inline-block;
+	
+	// we want rounded corners no matter the size of the field
+	border-radius: 9999em;
+	
+	// style of the actual input field
+	*:not(span) {
+		position: relative;
+		display: inherit;
+		border-radius: inherit;
+		margin: 0;
+		border: none;
+		outline: none;
+		padding: 0 .325em;
+		z-index: 1; // needs to be above the :focus span
+		
+		// summon fancy shadow styles when focussed
+		&:focus + span {
+			opacity: 1;
+			transform: scale(1);
+		}
+	}
+	
+	// we don't animate box-shadow directly as that can't be done on the GPU, only animate opacity and transform for high performance animations.
+	span {
+		
+		transform: scale(.993, .94); // scale it down just a little bit
+		transition: transform .5s, opacity .25s;
+		opacity: 0; // is hidden by default
+		
+		position:absolute;
+		z-index: 0; // needs to be below the field (would block input otherwise)
+		margin:4px; // a bit bigger than .input padding, this prevents background color pixels shining through
+		left:0;
+		top:0;
+		right:0;
+		bottom:0;
+		border-radius: inherit;
+		pointer-events: none; // this allows the user to click through this element, as the shadow is rather wide it might overlap with other fields and we don't want to block those.
+		
+		// fancy shadow styles
+		box-shadow: inset 0 0 0 3px #fff,
+			0 0 0 4px #fff,
+			3px -3px 30px #1beabd, 
+			-3px 3px 30px #10abff;
+	}
+	
+}
+
+html {
+	font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+	line-height:1.5;
+	font-size:1em;
+}
+
+body {
+	text-align: center;
+	display:flex;
+	align-items: center;
+	justify-content: center;
+}
+
+html, body {
+	height:100%;
+}
+
+input {
+	font-family: inherit;
+	line-height:inherit;
+	color:#2e3750;
+	min-width:12em;
+}
+
+::placeholder {
+	color:#cbd0d5;
+}
+
+html::after {
+	content:'';
+	background: linear-gradient(21deg, #10abff, #1beabd);
+	height:3px;
+	width:100%;
+	position:absolute;
+	left:0;
+	top:0;
+}
 .button-50 {
   appearance: button;
   background-color: #000;
@@ -115,18 +216,20 @@ if ($_POST['submit'] != "") {
             <h5 class="card-header">Convert</h5>
             <div class='card-body'>
                 <div class="form-group">
-                    <label>paste your Google Drive ID</label>
+                    <center> 
                     <form action="" method="POST">
-            <input type="text" size="80" name="url" value="<?php
+                        <span class="input">
+            <input type="text" size="80" placeholder="Enter google drive public link" name="url" value="<?php
 if ($iframeid) {
     echo $_POST['url'];
 } else {
     echo "";
 }
-?>"/>
-            <button class="btn btn-default" input type="submit" value="Generate" name="submit" >Submit </button>
+                                                           ?>"/></span>
+            <button class="button-50" input type="submit" value="Generate" name="submit" >Submit </button>
         </form>
-        <div id="myElement">Paste the Google Drive ID and click the submit button.</div>
+        <div id="myElement"></div>
+                        </center>
 
 <div class="col-md-12">
         <div class='card' style='margin:40px;'>
